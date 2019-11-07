59b09d10b0252629c9e1d1ce25ae4b5a79611946
amcman <amcman@seas.upenn.edu>
2019-11-06 21:52:35 -0500

    Just deleted the comments

--- DataAnalysisRunner.java
+++ DataAnalysisRunner.java
@@ -6,12 +6,10 @@
 public class DataAnalysisRunner {
 
 	/**
-	 * Runs the methods in DataAnalysis to output the results. 
-	 * The user-chosen flight file to review can be inputted below.
-	 * The program will take approximately 10-20 seconds to run, and the
-	 * results will NOT be printed to the console. You will just get a message 
-	 * indicating that the analysis is complete.
-	 * @param args
+	 *
+     
+     
+     
 	 */
 	public static void main(String[] args) {
 		DataAnalysis dataAnalysis = new DataAnalysis();
